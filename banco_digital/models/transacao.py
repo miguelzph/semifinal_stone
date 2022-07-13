21594b5f9468eab8bbe7a3805c00e46ff312c2cb
miguelzph <miguelzph@gmail.com>
2022-07-13 16:34:03 -0300

    correcao de logica

--- banco_digital/models/transacao.py
+++ banco_digital/models/transacao.py
@@ -48,18 +48,20 @@ def criar_transacao_espelho(transacao):
 @receiver(pre_save, sender=Transacao)
 def transacao_create_handler(sender, instance, *args, **kwargs):
 
-    if instance.tipo_id.id in [1,3] and instance.status_id.id != 2: 
-        instance.valor = -instance.valor
+    if instance.status_id.id != 2:
+        if instance.tipo_id.id in [1,3]: 
+            instance.valor = -instance.valor
         
-        if not tem_saldo(instance.conta_cliente, float(instance.valor)):
-            instance.status_id = StatusTransacao.objects.get(id=2)
-            raise serializers.ValidationError('A conta não possui saldo suficiente!')
-    print('alo')
-    modificar_valor_conta(instance.conta_cliente, float(instance.valor))
+            if not tem_saldo(instance.conta_cliente, float(instance.valor)):
+                instance.status_id = StatusTransacao.objects.get(id=2)
+                instance.save()
+                raise serializers.ValidationError('A conta não possui saldo suficiente!')
+
+        modificar_valor_conta(instance.conta_cliente, float(instance.valor))
     
-    if instance.tipo_id.id == 3:
-        # criar transferencia representativa do cliente que está recebendo
-        criar_transacao_espelho(instance)
+        if instance.tipo_id.id == 3:
+            # criar operação representativa do cliente que está recebendo
+            criar_transacao_espelho(instance)
 
             
             
