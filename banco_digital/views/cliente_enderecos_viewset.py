079ec2a4a4da47931ef0243661ca7b7ea28b75b7
miguelzph <miguelzph@gmail.com>
2022-07-17 01:37:39 -0300

    correcao de atualizaocao de email no cliente enderecos

--- banco_digital/views/cliente_enderecos_viewset.py
+++ banco_digital/views/cliente_enderecos_viewset.py
@@ -6,14 +6,18 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import CreateModelMixin
 
 
+
 class ClienteEnderecosViewSet(CreateModelMixin, GenericViewSet):
     queryset = ClienteEnderecos.objects.all()
     serializer_class = ClienteEnderecosSerializer
 
     def create(self, request, *args, **kwargs):
         """Função que cria um endereco para um cliente"""
+        
         # padroniza email minusculo
         if request.data.get("cliente_id"):
+            request.data._mutable = True
             request.data["cliente_id"] = request.data["cliente_id"].lower()
+            request.data._mutable = False
 
         return super().create(request, *args, **kwargs)
